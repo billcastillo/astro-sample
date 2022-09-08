9ae9307ac63fea05604d6e9c8e17229026b37999
Bill Castillo <bill.castillo@accenture.com>
2022-09-08 09:06:47 +0800

    initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,21 @@
+import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/serverless';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import image from '@astrojs/image';
+
+import tailwind from '@astrojs/tailwind';
+
+// https://astro.build/config
+export default defineConfig({
+  markdown: {
+    shikiConfig: {
+      theme: 'dracula',
+      langs: ['js', 'html'],
+    },
+  },
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap(), tailwind(), image()],
+  output: 'server',
+  adapter: vercel(),
+});
