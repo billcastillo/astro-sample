89c3efab45462d1f8743391b14dfd35ff30dff16
Bill <castillobil@gmail.com>
2022-10-08 17:47:22 +0800

    initial commit

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,20 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'BIR Gaming';
+export const SITE_DESCRIPTION = 'Pinakamabait na guild sa Ran Online Pinas. bagsakan ng mga pinakamatitikas at matatag na players. ';
+export const RAN_PINAS = {
+  max_level: 160
+};
+export const RAN_PINAS_SOCIAL = {
+  ran_pinas: {
+    title: 'RAN Online Pinas',
+    type: 'facebook',
+    url: 'https://www.facebook.com/ranonlinepinas'
+  },
+  ran_pinas_strife: {
+    title: 'RAN Online Strife',
+    type: 'facebook',
+    url: 'https://www.facebook.com/groups/ranpinasstrife'
+  }
+}
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,13 +1,41 @@
 module.exports = {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
       fontFamily: {
-        changa: 'Changa, sans-serif',
+        changa: "Changa, sans-serif",
+        bebas: "Bebas Neue",
+        verdana: [
+          "Tahoma",
+          "Verdana",
+          "system-ui",
+          "-apple-system",
+          "BlinkMacSystemFont",
+          "Segoe UI",
+          "Arial",
+          "Roboto",
+          "Oxygen",
+          "Ubuntu",
+          "Fira Sans",
+          "Droid Sans",
+          "Helvetica Neue",
+          "sans-serif",
+        ],
+      },
+      colors: {
+        "bir-red": "#FF0000",
+        "bir-dark": "#0E0E0E",
+        "bir-text-green": "#06D7B1",
+        "bir-text-gray": "#E3C8C8",
+      },
+      backgroundImage: {
+        "red-pic": "url('/bg/bg-red.jpg')",
+        "texture-and-cross":
+          " url('/bg/pattern-cross.png'), url('/bg/texture-1.jpg')",
+        "texture-1": "url('/bg/texture-1.jpg')",
+        "pattern-1": "url('/bg/pattern-cross.png')",
       },
     },
   },
-  plugins: [
-    require('@tailwindcss/typography'),
-  ],
+  plugins: [require("@tailwindcss/typography")],
 };
