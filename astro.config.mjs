e6411490b0c1087d403c1c47ca2f0217418d5e88
Bill <castillobil@gmail.com>
2022-10-08 18:13:14 +0800

    modify particles

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,6 @@ import sitemap from '@astrojs/sitemap';
 import image from '@astrojs/image';
 import tailwind from '@astrojs/tailwind';
 
-
-// https://astro.build/config
 export default defineConfig({
   markdown: {
     shikiConfig: {
