2464ff8c312528c3d3730f931394ce7465816b9b
Bill <castillobil@gmail.com>
2022-10-14 02:22:06 +0800

    add guides

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,28 @@
-import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import image from '@astrojs/image';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from 'astro/config'
+import vercel from '@astrojs/vercel/serverless'
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
+import image from '@astrojs/image'
+import tailwind from '@astrojs/tailwind'
 
 export default defineConfig({
   markdown: {
     shikiConfig: {
       theme: 'dracula',
-      langs: ['js', 'html']
-    }
+      langs: ['js', 'html'],
+    },
   },
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), image(), tailwind()],
+  integrations: [
+    mdx(),
+    sitemap(),
+    image(),
+    [
+      tailwind({
+        config: { applyBaseStyles: false },
+      }),
+    ],
+  ],
   output: 'server',
-  adapter: vercel()
-});
+  adapter: vercel(),
+})
