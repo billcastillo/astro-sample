f0752f9a6f07305110cd446e682410a81372008e
bill <castillobil@gmail.com>
2024-08-03 03:09:36 +0800

    3rd anniv update

--- src/config.ts
+++ src/config.ts
@@ -1,64 +1,75 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'BIR Gaming';
-export const SITE_DESCRIPTION = 'Pinakamabait na guild sa Ran Online Pinas. bagsakan ng mga pinakamatitikas at matatag na players. ';
+export const SITE_TITLE = 'BIR Gaming'
+export const SITE_DESCRIPTION =
+  'Pinakamalakas na guild sa Ran Online Pinas. bagsakan ng mga pinakamatitikas at matatag na players. '
 
 export const RAN_PINAS = {
-  max_level: 170
-};
+  max_level: 170,
+}
 
 export const RAN_PINAS_SOCIAL = {
+  bir_guild: {
+    title: 'BIR Guild',
+    type: 'facebook',
+    url: 'https://www.facebook.com/profile.php?id=61558626726323',
+  },
   ran_pinas: {
     title: 'RAN Online Pinas',
     type: 'facebook',
-    url: 'https://www.facebook.com/ranonlinepinas'
+    url: 'https://www.facebook.com/ranonlinepinas',
   },
   ran_pinas_strife: {
     title: 'RAN Online Strife',
     type: 'facebook',
-    url: 'https://www.facebook.com/groups/ranpinasstrife'
-  }
-};
+    url: 'https://www.facebook.com/groups/ranpinasstrife',
+  },
+}
 
 export const BOSS_TYPES = [
   {
     name: 'Dark Arts Master',
     id: 'DAM',
-    respawn_time: 60
+    respawn_time: 60,
   },
   {
     name: 'Cruel Jupiter',
     id: 'CJ',
-    respawn_time: 480
+    respawn_time: 480,
   },
   {
     name: 'Corrupted Bumbero',
     id: 'CB',
-    respawn_time: 40
+    respawn_time: 40,
   },
   {
     name: 'Bilanggo',
     id: 'BL',
-    respawn_time: 720
+    respawn_time: 720,
   },
   {
     name: 'Astral Swordsman',
     id: 'AS',
-    respawn_time: 720
+    respawn_time: 720,
   },
   {
     name: 'Crimson Swordsman',
     id: 'CS',
-    respawn_time: 720
+    respawn_time: 720,
   },
   {
     name: 'Dark Arts Agent',
     id: 'DAA',
-    respawn_time: 720
+    respawn_time: 720,
   },
-];
+]
 
-export const BIR_SENIORS = [
-  'KINDNESS#42520',
-];
+export const BIR_SENIORS = ['KINDNESS#42520', 'zillion#202368']
+
+export const STREAMERS = [
+  {
+    name: 'ADOR',
+    link: 'https://www.facebook.com/profile.php?id=100008440574329',
+  },
+]
